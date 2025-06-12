ecf247d4522ae01ccb99d32584f14d8c59405bed
fujiwara5 <sheee1ku@gmail.com>
2025-06-12 21:35:51 +0900

    Google認証の追加

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,7 +1,7 @@
 // js/supabase-config.js
 
 // ステップ1-4で取得した自分のプロジェクトのURLとAPIキーに書き換える
-const SUPABASE_URL = 'https://supabase.com/dashboard/project/ujdpwwhyftxwziebldxf'; 
+const SUPABASE_URL = 'https://ujdpwwhyftxwziebldxf.supabase.co'; 
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqZHB3d2h5ZnR4d3ppZWJsZHhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3MjI0NjAsImV4cCI6MjA2NTI5ODQ2MH0.WBYiiCYi08IHkIjdoGpEejT6v_XogG9T6tVQDJ6XgJc';
 
 // main.jsで使えるように変数をエクスポートする
